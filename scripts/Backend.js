295caa4e35efca817c58f50a63967af9677e9419
mohsenabedelaal <mohsen.abedelaal@gmail.com>
2022-04-13 04:15:26 +0300

    Fixed the navbar collapse .

--- scripts/Backend.js
+++ scripts/Backend.js
@@ -1,5 +1,5 @@
-var api = 'https://script.google.com/macros/s/AKfycbyxGXTQj4It4VMf0qksXq6DLmg1wVXDLzT6spU97zhiNJ5HJwL9A90-TyXONl2qX_jZaQ/exec'
-
+var api =
+  "https://script.google.com/macros/s/AKfycbyxGXTQj4It4VMf0qksXq6DLmg1wVXDLzT6spU97zhiNJ5HJwL9A90-TyXONl2qX_jZaQ/exec";
 
 // update 2022
 // var cv = {
@@ -32,43 +32,53 @@ var api = 'https://script.google.com/macros/s/AKfycbyxGXTQj4It4VMf0qksXq6DLmg1wV
 //     ]
 
 // }
-var data = {}
-var spinner = document.getElementById('loader')
+var data = {};
+var spinner = document.getElementById("loader");
 
 function LoadFromDB() {
-    //step 1 is to fetch data from gsheets
-    fetch(api).then(res => res.json()).then(res => {
+  //step 1 is to fetch data from gsheets
+  fetch(api)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      data = res;
+      console.log("data = ", data);
 
-        console.log(res)
-        data = res
-        console.log('data = ', data)
+      // createskill(data.skills)
+      // createprojects(data.projects)
+      // turn off the spinner
+    });
 
-
-        
-        
-            // createskill(data.skills)
-        // createprojects(data.projects)
-            // turn off the spinner 
-        
-    })
-
-    fetch('https://script.google.com/macros/s/AKfycbxk0ol26QH89YTWOViFNx9FdXsfrTZRGp7SZogSFSnreVTBq5Rs6FNgv-QqqeWHMGxu/exec')
-    .then(res=>res.json()).then(res=> {
-        data = res
-        console.log(res)
-        document.getElementById('website_name').innerText = data['header-content']['website-name']
-        document.getElementById('logo').src = data['header-content']['profile_pic']
-        document.getElementById('h1').innerText = data['header-content']['full_name']
-        document.getElementById('h2').innerText = data['header-content']['job_title']
-        document.getElementById('cv').href = data['header-content']['cv']
-        document.getElementById('email').innerText = data['about-me-content']['email']
-        document.getElementById('number').innerText = data['about-me-content']['phone_number']
-        document.getElementById('location').innerText = data['about-me-content']['location']
-        document.getElementById('lang').innerText = data['about-me-content']['language']
-        document.getElementById('nabtha').innerText = data['about-me-content']['about_me_details']
-        createprojects(data.projects)
-        spinner.style.display = 'none'
-    })
+  fetch(
+    "https://script.google.com/macros/s/AKfycbxk0ol26QH89YTWOViFNx9FdXsfrTZRGp7SZogSFSnreVTBq5Rs6FNgv-QqqeWHMGxu/exec"
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      data = res;
+      console.log(res);
+    //   document.getElementById("website_name").innerText =
+    //     data["header-content"]["website-name"];
+      document.getElementById("logo").src =
+        data["header-content"]["profile_pic"];
+      document.getElementById("h1").innerText =
+        data["header-content"]["full_name"];
+      document.getElementById("h2").innerText =
+        data["header-content"]["job_title"];
+      document.getElementById("cv").href = data["header-content"]["cv"];
+      document.getElementById("email").innerText =
+        data["about-me-content"]["email"];
+      document.getElementById("number").innerText =
+        data["about-me-content"]["phone_number"];
+      document.getElementById("location").innerText =
+        data["about-me-content"]["location"];
+      document.getElementById("lang").innerText =
+        data["about-me-content"]["language"];
+      document.getElementById("nabtha").innerText =
+        data["about-me-content"]["about_me_details"];
+      createprojects(data.projects);
+    //   createskill()
+      spinner.style.display = "none";
+    });
 }
 
 // function createskill(allskills) {
@@ -82,7 +92,6 @@ function LoadFromDB() {
 //         var colprogress = document.createElement('div')
 
 //         var skillbox = document.createElement('div')
-
 
 //         var eachprogress = document.createElement('div')
 //         eachprogress.classList.add('progress-container')
@@ -108,8 +117,6 @@ function LoadFromDB() {
 //         skillprogressbar.setAttribute("aria-valuemax", "100");
 //         skillprogressbar.setAttribute("style", "width: 80%;");
 
-
-
 //         var skillperc = document.createElement('span')
 //         skillperc.innerText = s.perc
 //         skillperc.classList.add('progress-value')
@@ -120,46 +127,196 @@ function LoadFromDB() {
 //         document.getElementById('skillbox').appendChild(skillname)
 //         document.getElementById('skillbox').appendChild(skillprogress)
 
-
-
-
 //     });
-
 
 // }
 
 function createprojects(projects) {
-    var box = document.getElementById('pbox')
-    projects.forEach(proj => {
+  var box = document.getElementById("pbox");
+  projects.forEach((proj) => {
+    var div = document.createElement("div");
+    div.classList.add("card");
+    div.classList.add("col-md-3");
+    div.classList.add("mr-2");
+    div.classList.add("ml-2");
+    div.style.width = "15rem";
 
-        var div = document.createElement('div')
-        div.classList.add('card')
-        div.classList.add('col-md-3')
-        div.classList.add('mr-2')
-        div.classList.add('ml-2')
-        div.style.width = '15rem'
+    var img = document.createElement("img");
+    img.classList.add("card-img-top");
+    img.classList.add("mt-2");
+    img.src = proj.image;
+    img.alt = proj.title;
 
-        var img = document.createElement('img')
-        img.classList.add('card-img-top')
-        img.classList.add('mt-2')
-        img.src = proj.image
-        img.alt = proj.title
+    var h5 = document.createElement("h5");
+    h5.classList.add("card-title");
+    h5.classList.add("mt-1");
+    h5.innerText = proj.title;
 
-        var h5 = document.createElement('h5')
-        h5.classList.add('card-title')
-        h5.classList.add('mt-1')
-        h5.innerText = proj.title
+    var p = document.createElement("p");
+    p.classList.add("card-text");
+    p.classList.add("mt-1");
+    p.classList.add("mb-2");
+    p.innerText = proj.category;
 
-        var p = document.createElement('p')
-        p.classList.add('card-text')
-        p.classList.add('mt-1')
-        p.classList.add('mb-2')
-        p.innerText = proj.category
-
-        div.appendChild(img)
-        div.appendChild(h5)
-        div.appendChild(p)
-        box.appendChild(div)
-    });
-
+    div.appendChild(img);
+    div.appendChild(h5);
+    div.appendChild(p);
+    box.appendChild(div);
+  });
 }
+function createExperience(experiences) {
+  var box = document.getElementById("experience-card");
+  projects.forEach((proj) => {
+    var row = document.createElement("div");
+    row.classList.add("row");
+    var div = document.createElement("div");
+
+    div.classList.add("col-md-3");
+    div.classList.add("mr-2");
+    div.classList.add("ml-2");
+    div.style.width = "15rem";
+
+    var img = document.createElement("img");
+    img.classList.add("card-img-top");
+    img.classList.add("mt-2");
+    img.src = proj.image;
+    img.alt = proj.title;
+
+    var h5 = document.createElement("h5");
+    h5.classList.add("card-title");
+    h5.classList.add("mt-1");
+    h5.innerText = proj.title;
+
+    var p = document.createElement("p");
+    p.classList.add("card-text");
+    p.classList.add("mt-1");
+    p.classList.add("mb-2");
+    p.innerText = proj.category;
+
+    div.appendChild(img);
+    div.appendChild(h5);
+    div.appendChild(p);
+    box.appendChild(div);
+  });
+}
+
+function createskill() {
+
+  var testing = "dsadsa";
+  for(var i=0;i <= 3;i++){
+    var box = document.getElementById('experience')
+    var experience_card = document.getElementById('experience-card')
+    let clone_card = experience_card.cloneNode(true)
+    // clone_card.id = `experience-card-${i}`
+    box.appendChild(clone_card)
+  }
+ 
+}
+
+// function createskill(){
+
+//     var box = document.getElementById('experience')
+//     var card = document.createElement('div')
+//     card.classList.add('card')
+//     var div_inner = document.createElement('div')
+//     div_inner.classList.add('row')
+
+//     var div_duration = document.createElement('div')
+//     div_duration.classList.add('col-md-3')
+//     div_duration.classList.add('bg-primary')
+//     div_duration.setAttribute('data-aos','fade-right')
+//     div_duration.setAttribute('data-aos-offset','50')
+//     div_duration.setAttribute('data-aos-duration','500')
+
+//     var div_duration_value = document.createElement('div')
+//     div_duration_value.classList.add('card-body')
+//     div_duration_value.classList.add('cc-experience-header')
+
+//     var p_date = document.createElement('p')
+//     p_date.innerText = "dsadsa"
+//     var company_name = document.createElement('div')
+//     company_name.classList.add('h5')
+//     company_name.innerText = "dasdasdas"
+
+//     var information = document.createElement('div')
+//     information.classList.add('col-md-9')
+//     information.setAttribute('data-aos','fade-left')
+//     information.setAttribute('data-aos-offset','50')
+//     information.setAttribute('data-aos-duration','500')
+//     var information_value = document.createElement('div')
+//     information_value.classList.add('card-body')
+
+//     var job_title = document.createElement('div')
+//     job_title.classList.add('h5')
+//     job_title.innerText = "dasdsada"
+//     var details = document.createElement('p')
+//     details.innerText = "dasdsadasjkdhkjasd djkashdkjashjkdsa"
+
+//     card.appendChild(div_inner)
+//     div_inner.appendChild(div_duration)
+//     // div_duration.appendChild(div_duration_value)
+//     // div_duration_value.appendChild(p_date)
+//     // div_duration_value.appendChild(company_name)
+//     // information.appendChild(information_value)
+//     // div_inner.appendChild(information)
+//     // information_value.appendChild(job_title)
+//     // information_value.appendChild(details)
+//     box.appendChild(card)
+
+//     // row.classList.add('row')
+//     // var div = document.createElement('div')
+// //     projects.forEach(proj => {
+
+// //         var row = document.createElement('div')
+// //         row.classList.add('row')
+// //         var div = document.createElement('div')
+
+// //         div.classList.add('col-md-3')
+// //         div.classList.add('mr-2')
+// //         div.classList.add('ml-2')
+// //         div.style.width = '15rem'
+
+// //         var img = document.createElement('img')
+// //         img.classList.add('card-img-top')
+// //         img.classList.add('mt-2')
+// //         img.src = proj.image
+// //         img.alt = proj.title
+
+// //         var h5 = document.createElement('h5')
+// //         h5.classList.add('card-title')
+// //         h5.classList.add('mt-1')
+// //         h5.innerText = proj.title
+
+// //         var p = document.createElement('p')
+// //         p.classList.add('card-text')
+// //         p.classList.add('mt-1')
+// //         p.classList.add('mb-2')
+// //         p.innerText = proj.category
+
+// //         div.appendChild(img)
+// //         div.appendChild(h5)
+// //         div.appendChild(p)
+// //         box.appendChild(div)
+// // })
+
+// }
+
+// <div class="card" id="experience-card">
+//                             <div class="row">
+//                                 <div class="col-md-3 bg-primary" data-aos="fade-right" data-aos-offset="50"
+//                                     data-aos-duration="500">
+//                                     <div class="card-body cc-experience-header">
+//                                         <p>مارس 2016 - حتى الآن</p>
+//                                         <div class="h5">شركة ألف وباء</div>
+//                                     </div>
+//                                 </div>
+//                                 <div class="col-md-9" data-aos="fade-left" data-aos-offset="50" data-aos-duration="500">
+//                                     <div class="card-body">
+//                                         <div class="h5">اخصائي موارد بشرية</div>
+//                                         <p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من
+//                                             مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى
+//                                             إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </div>
